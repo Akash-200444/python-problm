7c1e8cb57b27bb035b7bb11d97d49c27e77dbaee
Akash-200444 <akashdhanush816@gmail.com>
2024-12-16 04:00:50 -0800

    Add files via upload

---
+++ ass.py
@@ -0,0 +1,14 @@
+def number(add):
+    if add % 2:
+         return  print (number,"is a add number")
+    else:
+         return print (number,"is a not add number")
+
+def num(even):
+    if even / 2:
+        return print (num,"is a even number")
+    else:
+        return print (num,"is a not even number")
+a=int(input("enter a number:"))
+o=number(a)
+q=num(a)
